f0f26beafc7e586a2649ce21ca4ceac06867e046
Chihiro Ito <hokousya@sourcewalker.com>
2021-12-18 15:40:51 +0900

    Simplified setup.py.

--- ext/setup.py
+++ ext/setup.py
@@ -1,4 +1,5 @@
 import os, sys
+from distutils.core import setup, Extension
 from pathlib import Path
 
 ROOT_PATH = Path(__file__).resolve().parent.parent.resolve()
@@ -6,16 +7,10 @@ ROOT_PATH = Path(__file__).resolve().parent.parent.resolve()
 if sys.platform == 'darwin':
     FRAMEWORKS = (ROOT_PATH / 'downloads/aqtk10_mac/AquesTalk.framework',
                   ROOT_PATH / 'downloads/aqk2k_mac/AqKanji2Koe.framework')
-    RPATH = ':'.join({ str(x.parent) for x in FRAMEWORKS })
-
     os.environ['CFLAGS'] = ' '.join(['-I%s' % (x / 'Headers') for x in FRAMEWORKS])
     os.environ['LDFLAGS'] = ' '.join(['-rpath %s -F%s -framework %s' % (x.parent, x.parent, x.stem)
                                       for x in FRAMEWORKS])
     os.environ['ARCHFLAGS'] = '-arch x86_64'
-
-from distutils.core import setup, Extension
-
-if sys.platform == 'darwin':
     module1 = Extension('aqtk', sources = ['aqtk.c'])
 else:
     module1 = Extension('aqtk',
